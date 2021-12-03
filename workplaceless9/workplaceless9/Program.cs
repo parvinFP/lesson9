a897cb54bd4d9ee5f45eec406566523dc22afc2f
[parvinFP] <[parvinfp@code.edu.az]>
2021-12-03 10:29:22 +0300

    lesson9

---
+++ workplaceless9/workplaceless9/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+
+namespace workplaceless9
+{
+    public class less9
+    {
+        public static void Main()
+        {
+            Employee employee = new Employee()
+            {
+                dailysalary = 500,
+                name = "pervin",
+                position = "programmer",
+                surname = "pasha"
+            };
+            employee.salary(employee.dailysalary);
+            int monthsal = employee.salary(employee.dailysalary);
+            Console.WriteLine(monthsal);
+        }
+    }
+}
---
+++ workplaceless9/workplaceless9/employee.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace workplaceless9
+{
+    public class Employee
+    {
+       public string name;
+        public string surname;
+        public string position;
+        public int dailysalary;
+        public int salary(int num)
+        {
+            return dailysalary * 30;
+        }
+        
+    }
+}
